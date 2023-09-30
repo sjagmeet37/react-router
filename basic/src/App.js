94d46fd2c9bbec247311f9589a21c855f7c07dfa
Jagmeet Sandhu <sjagmeet37@gmail.com>
2023-09-30 22:36:26 +0530

    adding basic route

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,5 +1,12 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+
+const routes = createBrowserRouter([
+  {path : '/', element : <Home />}
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={routes}/>;
 }
 
 export default App;
---
+++ basic/src/pages/Home.js
@@ -0,0 +1,6 @@
+const Home = () => {
+
+    return (<p>This is Home Companent</p>)
+}
+
+export default Home;   
