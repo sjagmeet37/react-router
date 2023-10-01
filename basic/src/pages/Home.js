d9453b23bd17936d84cae6d13a667a7b19902c84
Jagmeet Sandhu <sjagmeet37@gmail.com>
2023-10-01 09:11:40 +0530

    defining a link

--- basic/src/pages/Home.js
+++ basic/src/pages/Home.js
@@ -1,6 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
+  return (
+    <>
+      <p>This is Home Companent</p>
+      <Link to="/customers">Click here to go to customers page</Link>
+    </>
+  );
+};
 
-    return (<p>This is Home Companent</p>)
-}
-
-export default Home;   
+export default Home;
