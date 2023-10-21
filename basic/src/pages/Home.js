2b97c4a5a1a801bb22dac52284adc4f294644db9
Jagmeet Sandhu <sjagmeet37@gmail.com>
2023-10-21 22:41:46 +0530

    navigating programmatically

--- basic/src/pages/Home.js
+++ basic/src/pages/Home.js
@@ -1,10 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Home = () => {
+
+  const navigate = useNavigate();
+  const navigateToCustomer = () => {
+    navigate('/customers');
+  }
+
   return (
     <>
       <p>This is Home Companent</p>
       <Link to="/customers">Click here to go to customers page</Link>
+      <button onClick={navigateToCustomer}>Home</button>
     </>
   );
 };
